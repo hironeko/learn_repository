c149f69a9069f14d775d9263b78717a7ebace62b
hironeko <hironeko.sun@gmail.com>
2019-05-28 23:13:43 +0900

    delete project

--- javascript/sample_nuxt/pages/about.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <section class="hero is-primary is-bold">
-    <div class="hero-body">
-      <h1 class="title is-size-2">About</h1>
-      <h2 class="subtitle is-size-4">sample nuxtとは？</h2>
-    </div>
-  </section>
-</template>
