39431503456aee85410f447b561bc8ec1d0d1de0
hironeko <hironeko.sun@gmail.com>
2019-07-09 23:33:12 +0900

    自身が書いたものへ修正

--- javascript/nuxt_socket_io/nuxt/pages/socket.vue
+++ javascript/nuxt_socket_io/nuxt/pages/socket.vue
@@ -60,105 +60,45 @@ export default {
     }
   },
   computed: {
-    // 配列の後ろ（新しいもの）から順に表示させたいので反転させる
     reverseMessages: function() {
       return this.messages.slice().reverse()
     }
   },
   mounted() {
-    // VueインスタンスがDOMにマウントされたらSocketインスタンスを生成する
     this.socket = io()
 
-    // サーバー側で保持しているメッセージを受信する
     this.socket.on("new-message", message => {
       this.messages.push(message || {})
     })
 
-    // コンポーネントがマウントされてから1秒間はローディングする
     setTimeout(() => {
       this.isLoading = false
     }, 1000)
   },
   methods: {
     sendMessage() {
-      // スペースのみの場合は即時リターンをする
       if (!this.message.trim()) {
         return
       }
 
-      let now = new Date() // 現在時刻（世界標準時）を取得
-      now.setTime(now.getTime() + 1000 * 60 * 60 * 9) // 日本時間に変換
+      let now = new Date()
+      now.setTime(now.getTime() + 1000 * 60 * 60 * 9) //日本時間に変換
       now = now
         .toJSON()
         .split("T")[1]
-        .slice(0, 5) // 時刻のみを取得
+        .slice(0, 5)
 
-      // メッセージオブジェクトを作る
       let message = {
         user: this.socket.id,
         date: now,
         text: this.message.trim()
       }
-
-      // 自身（Vueインスタンス）のデータオブジェクトにメッセージを追加する
       this.messages.push(message)
-      // サーバー側にメッセージを送信する
       this.socket.emit("send-message", message)
-      // input要素を空にする
       this.message = ""
     }
   }
 }
-
-// export default {
-//   date() {
-//     return {
-//       message: "",
-//       messages: [],
-//       socket: "",
-//       isLoading: true
-//     }
-//   },
-//   computed: {
-//     reverseMessages: function() {
-//       return this.messages.slice().reverse()
-//     }
-//   },
-//   mounted() {
-//     this.socket = io()
-
-//     this.socket.on("new-message", message => {
-//       this.messages.push(message || {})
-//     })
-
-//     setTimeout(() => {
-//       this.isLoading = false
-//     }, 1000)
-//   },
-//   methods: {
-//     sendMessage() {
-//       if (!this.message.trim()) {
-//         return
-//       }
-
-//       let now = new Date()
-//       now.setTime(now.getTime() + 1000 * 60 * 60 * 9) //日本時間に変換
-//       now = now
-//         .toJSON()
-//         .split("T")[1]
-//         .slice(0, 5)
-
-//       let message = {
-//         user: this.socket.id,
-//         date: now,
-//         text: this.message.trim()
-//       }
-//       this.messages.push(message)
-//       this.socket.emit("send-message", message)
-//       this.message = ""
-//     }
-//   }
-// }
 </script>
 
 <style scoped>
