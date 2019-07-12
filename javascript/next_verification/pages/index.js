450707537bcc47c21cbd96bf2dda9a1a657484f0
hironeko <hironeko.sun@gmail.com>
2019-07-13 01:50:06 +0900

    readme の追加

--- javascript/next_verification/pages/index.js
+++ javascript/next_verification/pages/index.js
@@ -1,9 +1,21 @@
 import Layout from "../components/Layout"
+import Link from "next/link"
 
-const Index = () => (
-  <Layout>
-    <p>Hello Next.js</p>
-  </Layout>
+const PostLink = props => (
+  <li>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
 )
 
-export default Index
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="learn-nextjs" title="Lear Next.js is awasome" />
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+    </ul>
+  </Layout>
+)
