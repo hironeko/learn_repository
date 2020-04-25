7c41973a8fcc56d5225afb3c482ac933b379ef96
hironeko <hironeko.sun@gmail.com>
2020-04-25 21:32:29 +0900

    表示できるように修正

--- javascript/learn_nextjs_2020/next-blog-tutorial/pages/index.js
+++ javascript/learn_nextjs_2020/next-blog-tutorial/pages/index.js
@@ -28,7 +28,7 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => {
+          {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               {title}
               <br />
@@ -36,7 +36,7 @@ export default function Home({ allPostsData }) {
               <br />
               {date}
             </li>
-          })}
+          ))}
         </ul>
       </section>
     </Layout>
