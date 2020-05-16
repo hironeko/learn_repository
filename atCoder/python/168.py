999e86b43e7c93293c37643d9d905f6a176b20da
hironeko <hironeko.sun@gmail.com>
2020-05-16 09:59:38 +0900

    AtCoder168

---
+++ atCoder/python/168.py
@@ -0,0 +1,19 @@
+# Aの解答
+# n = str(input())
+# n1 = int(n[-1])
+# if n1 in [2, 4, 5, 7, 9]:
+#     print('hon')
+# elif n1 in [0, 1, 6, 8]:
+#     print('pon')
+# elif n1 == 3:
+#     print('bon')
+# Bの問題
+# k = int(input())
+# s = input()
+
+# if len(s) <= k:
+#     print(s)
+# else:
+#     print(s[0:k] + '...')
+
+# Cの問題
