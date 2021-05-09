3e806b5de083764383a7fa9e67f436ce73855c39
hironeko <hironeko.sun@gmail.com>
2021-05-10 00:40:17 +0900

    fix

--- typescript/first_learn/src/app.ts
+++ typescript/first_learn/src/app.ts
@@ -1,68 +1,94 @@
-console.log("Hello! Node.js * TypeScript");
-let message: String = 'TEST';
+// console.log("Hello! Node.js * TypeScript");
+// let message: String = 'TEST';
 
-function add(a: string, b: number ): string {
-  return a + b;
-}
+// function add(a: string, b: number ): string {
+//   return a + b;
+// }
 
-let str: string = '検証';
-let num: number = 6789;
+// let str: string = '検証';
+// let num: number = 6789;
 
-console.log(add(str, num));
+// console.log(add(str, num));
 
-type sumStr = {
-  x: string;
-  y: number;
- }
+// type sumStr = {
+//   x: string;
+//   y: number;
+//  }
 
-//let test: sumStr = {
-//  x: 'test',
-//  y: 1988
-//}
+// //let test: sumStr = {
+// //  x: 'test',
+// //  y: 1988
+// //}
 
-//console.log(test)
+// //console.log(test)
+
+// interface User {
+//   id: number,
+//   name: string,
+//   age: number
+// };
+
+// const user: User = {
+//   id: 1,
+//   name: 'test 太郎',
+//   age: 12
+// };
+
+// console.log(user);
+
+
+
+
+
+// class Test {
+//   private first: number;
+//   private second: number;
+
+//   constructor (first: number, second: number) {
+//     this.first = first;
+// 	this.second = second;
+//   }
+
+//   public sum(): string {
+//     const sum: number = this.first + this.second;
+//     return `${this.first} + ${this.second}は、${sum}です。`;
+//   }
+// }
+
+
+// const test = new Test(100, 200);
+
+// console.log(test.sum())
+
 
 interface User {
-  id: number,
-  name: string,
+  name: string
   age: number
-};
+  address: string
+}
 
 const user: User = {
-  id: 1,
-  name: 'test 太郎',
-  age: 12
-};
+  name: '検証',
+  age: 23,
+  address: '埼玉'
+}
 
-console.log(user);
-
-
-
-
-
-class Test {
-  private first: number;
-  private second: number;
-
-  constructor (first: number, second: number) {
-    this.first = first;
-	this.second = second;
-  }
-
-  public sum(): string {
-    const sum: number = this.first + this.second;
-	return `${this.first} + ${this.second}は、${sum}です。`;
-  }
+const user2: User = {
+  name: 'や',
+  age: 23,
+  address: 'や'
 }
 
 
-const test = new Test(100, 200);
+// console.log(user)
 
-console.log(test.sum())
-
-
-
-
+interface UserList {
+  items: User[]
+}
 
 
+const userList: UserList = {
+  items: [user, user2]
+}
 
+console.log(userList)
